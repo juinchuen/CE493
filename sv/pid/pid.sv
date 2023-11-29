af7d3d958e41e13cd71191ca104bf6bfb63e3cb9
Johnny Chen <johnnychen2023@u.northwestern.edu>
2023-11-29 15:49:06 -0600

    PID but it compiles

--- sv/pid/pid.sv
+++ sv/pid/pid.sv
@@ -1,6 +1,6 @@
-module PID #(
+module pid #(
     parameter D_WIDTH = 32,
-    parameter Q_BITS = 10
+    parameter Q_BITS = 10,
     parameter LIM_MAX = 100,
     parameter LIM_MIN = -100
 ) (
@@ -14,27 +14,29 @@ module PID #(
     output logic signed [D_WIDTH-1:0] out
 );
 
-logic [D_WIDTH-1:0] i_error, i_error_c, d_error;
-logic [D_WIDTH-1:0] kp, ki, kd_1, kd_2
+logic signed [D_WIDTH-1:0] error, prev_error, p_error;
+logic signed [D_WIDTH-1:0] i_error, i_error_c, d_error, prev_d_error;
+logic [D_WIDTH-1:0] kp, ki, kd_1, kd_2;
+logic signed [D_WIDTH-1:0] lim_max_int_c, lim_min_int_c;
 
 always_ff @(posedge clock or posedge reset) begin
     if(!reset) begin
-        lim_min <= '0;
-        lim_max <= '0;
+        lim_min_int_c <= '0;
+        lim_max_int_c <= '0;
         i_error <= '0;
         prev_error <= '0;
+        prev_d_error <= '0;
     end else begin
-        lim_min <= lim_min_c;
-        lim_max <= lim_max_c;
         i_error <= i_error_c;
         prev_error <= error;
+        prev_d_error <= d_error;
     end
     if (!write_enable) begin
-        unique case (reg_addr)
-            0 : kp <= reg_data
-            1 : ki <= reg_data
-            2 : kd_1 <= reg_data
-            3 : kd_2 <= reg_data
+        case (reg_addr)
+            0 : kp <= reg_data;
+            1 : ki <= reg_data;
+            2 : kd_1 <= reg_data;
+            3 : kd_2 <= reg_data;
         endcase
     end
 end
@@ -49,17 +51,11 @@ anti windup
 clamp integrator
 */
 
-logic signed [D_WIDTH-1:0] error, prev_error, p_error;
-logic signed [D_WIDTH-1:0] i_error, i_error_c, d_error, prev_d_error;
-logic signed [D_WIDTH-1:0] lim_max_int, lin_max_int_c;
-logic signed [D_WIDTH-1:0] lim_min_int, lin_min_int_c;
-
-
 always_comb begin
     error = target - measurement;
-    p_error = error * Kp;
-    i_error_c = i_error + Ki * (error + prev_error)
-    d_error = kd_1 * (error - prev_error) + kd_2 * prev_d_error
+    p_error = error * kp;
+    i_error_c = i_error + ki * (error + prev_error);
+    d_error = kd_1 * (error - prev_error) + kd_2 * prev_d_error;
     
     // anti windup via dynamic integrator clamping
     if (LIM_MAX > p_error)
