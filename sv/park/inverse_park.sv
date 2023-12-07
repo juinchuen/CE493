841b65a9f4df448bd4f8a45d042291b5e1ac8b20
jurgz42 <jurgentisza@gmail.com>
2023-12-07 03:39:28 -0600

    Update inverse_park.sv

--- sv/park/inverse_park.sv
+++ sv/park/inverse_park.sv
@@ -28,7 +28,8 @@ cos and sin values need to be quantized
 */
 
 logic signed [D_WIDTH-1:0] alpha_c, beta_c;
-logic signed [D_WIDTH*2-1:0] D_cos, D_sin, Q_sin, Q_cos; 
+logic signed [D_WIDTH*2-1:0] D_cos_c, D_sin_c,
+logic signed [D_WIDTH*2-1:0] Q_sin_c, Q_cos_c; 
 logic done_c;
 
 enum logic {s0, s1} state, next_state;
@@ -39,11 +40,19 @@ always_ff @(posedge clk or negedge rstb) begin
     beta <= 'b0;
     done <= 'b0;
     state <= s0;
+    D_cos <= 'b0;
+    D_sin <= 'b0;
+    Q_sin <= 'b0;
+    Q_cos <= 'b0;
   end else begin
     alpha <= alpha_c;
     beta <= beta_c;
     done <= done_c;
     state <= next_state;
+    D_cos <= D_cos_c;
+    D_sin <= D_sin_c;
+    Q_sin <= Q_sin_c;
+    Q_cos <= Q_cos_c;
   end
 end
 
@@ -52,14 +61,18 @@ always_comb begin
   beta_c <= beta;
   done_c <= done;
   next_state <= state;
+  D_cos_c <= D_cos;
+  D_sin_c <= D_sin;
+  Q_sin_c <= Q_sin;
+  Q_cos_c <= Q_cos;
 
   case(state)
     s0: begin
       if (start) begin
-        D_cos = (D * cos) >>> Q_BITS;
-        D_sin = (D * sin) >>> Q_BITS;
-        Q_sin = (Q * sin) >>> Q_BITS;
-        Q_cos = (Q * cos) >>> Q_BITS;
+        D_cos_c = (D * cos) >>> Q_BITS;
+        D_sin_c = (D * sin) >>> Q_BITS;
+        Q_sin_c = (Q * sin) >>> Q_BITS;
+        Q_cos_c = (Q * cos) >>> Q_BITS;
         // alpha_c = ((D * cos) - (Q * sin)) >>> Q_BITS;
         // beta_c = ((D * sin) - (Q * cos)) >>> Q_BITS;
         
