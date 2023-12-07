833c609e635c2f5aff683aaffc9ee5c0a55081c3
jurgz42 <jurgentisza@gmail.com>
2023-12-07 03:25:57 -0600

    Update inverse_park.sv

--- sv/park/inverse_park.sv
+++ sv/park/inverse_park.sv
@@ -28,17 +28,22 @@ cos and sin values need to be quantized
 */
 
 logic signed [D_WIDTH-1:0] alpha_c, beta_c;
+logic sifned [D_WIDTH*2-1:0] D_cos, D_sin, Q_sin, Q_cos; 
 logic done_c;
+
+enum logic {s0, s1} state, next_state;
 
 always_ff @(posedge clk or negedge rstb) begin
   if (!rstb) begin
     alpha <= 'b0;
     beta <= 'b0;
     done <= 'b0;
+    state <= s0;
   end else begin
     alpha <= alpha_c;
     beta <= beta_c;
     done <= done_c;
+    state <= next_state;
   end
 end
 
@@ -47,11 +52,27 @@ always_comb begin
   beta_c <= beta;
   done_c <= done;
 
-  if (start) begin
-    alpha_c = ((D * cos) - (Q * sin)) >>> Q_BITS;
-    beta_c = ((D * sin) - (Q * cos)) >>> Q_BITS;
-    done_c <= 'b1;
-  end
+  case(state)
+    s0: begin
+      if (start) begin
+        D_cos = D * cos;
+        D_sin = D * sin;
+        Q_sin = Q * sin;
+        Q_cos = Q * cos;
+        // alpha_c = ((D * cos) - (Q * sin)) >>> Q_BITS;
+        // beta_c = ((D * sin) - (Q * cos)) >>> Q_BITS;
+        
+        next_state <= s1;
+      end
+    end
+
+    s1: begin
+      alpha_c <= (D_cos - Q_sin) >>> Q_BITS;
+      beta_c <= (D_sin - Q_cos) >>> Q_BITS;
+      done_c <= 'b1;
+      next_state <= s0;
+    end
+  endcase;
 end
     
 endmodule
