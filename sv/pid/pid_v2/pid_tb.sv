7ce6d17e6cd6e1d51c7d67b67ce731158ca32230
juinchuen <juinoh2024@u.northwestern.edu>
2024-01-11 11:34:05 -0600

    tb based on c prototype

--- sv/pid/pid_v2/pid_tb.sv
+++ sv/pid/pid_v2/pid_tb.sv
@@ -6,38 +6,84 @@ module pid_tb #(
 )();
 
     reg clk, rstb;
-    reg write_enable;
-    reg iterate_enable;
+    reg write_enable, iterate_enable;
 
-    reg [D_WIDTH-1:0] reg_addr;
-    reg [D_WIDTH-1:0] reg_data;
-    reg [D_WIDTH-1:0] target;
-    reg [D_WIDTH-1:0] measurement;
+    reg [D_WIDTH-1:0] reg_addr, reg_data;
+    reg [D_WIDTH-1:0] variable;
 
-    wire out;
-    wire out_valid;
+    wire [D_WIDTH-1:0]  out;
+    wire                out_valid;
     
-    int stimulus;
-
+    int stimulus, results;
     int stim1, stim2, stim3;
+
+    always #5 clk = ~clk;
 
     initial begin
 
-        stimulus = $fopen("../c_prototype/pid_test_q.csv", "r");
+        $display("Opening stimulus file and results file");
 
-        while (!$feof(stimulus)) begin
+        stimulus = $fopen("../c_prototype/pid_test_q.csv", "r");
+        results  = $fopen("pid_sim.csv", "w");
+
+        clk             = 0;
+        rstb            = 1; // active low
+        write_enable    = 1; // active low
+        iterate_enable  = 0;
+        reg_addr        = 0;
+        reg_data        = 0;
+        variable        = 0;
+
+        #0 rstb = 0;
+        #0 rstb = 1;
+
+        reg_addr = 0;
+        reg_data = (32'h1) << 25;
+        #10
+
+        reg_addr = 1;
+        reg_data = (32'h1) << 28;
+        #10
+
+        iterate_enable = 1;
+
+        while (!feof(stimulus)) begin
 
             $fscanf(stimulus, "%d, %d, %d\n", stim1, stim2, stim3);
 
-            $display("read %d\n", stim1);
+            wait(out_valid);
+
+            fprintf(results, "%d, %d, %d\n", stim1, out, variable);
+
+            variable = variable + out;
+
+            #10
 
         end
 
-        $display("Done reading\n");
+        $display("Simulation Done\n");
+
+        $fclose(stimulus);
+        $fclose(results);
 
         $finish;
 
     end
 
+    pid #(
+        .D_WIDTH    (D_WIDTH),
+        .Q_BITS     (Q_BITS)
+    ) pid0 (
+        .clock          (clk),
+        .reset          (rstb),
+        .write_enable   (write_enable),
+        .iterate_enable (iterate_enable),
+        .reg_addr       (reg_addr),
+        .reg_data       (reg_data),
+        .target         (stim1),
+        .measurement    (variable),
+        .out            (out),
+        .out_valid      (out_valid)
+    );
 
 endmodule
